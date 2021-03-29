86f2bc5eb788018332c7a074f984b26998c5087c
Christer <christerforsgren91@gmail.com>
2021-03-29 20:22:02 +0200

    added testing framework for withdrawl function, test failed because of no implementation code

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 
 require './lib/atm.rb'
 describe Atm do
+    let(:account) { instance_double('Account')} 
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+    
     it 'has 1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -8,6 +15,11 @@ describe Atm do
     it 'funds are withdrew at withdrawl' do
         subject.withdrawl 50
         expect(subject.funds).to eq 950
+    end
+
+    it 'allow withdrawl if the account has enough balance.' do
+        expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
+        expect(subject.withdrawl(45, account)).to eq expected_output
     end
 
 
