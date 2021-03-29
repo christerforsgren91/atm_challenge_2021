0f850b30803c975611c52dc2eb7abe754e981ef9
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-29 20:01:21 +0200

    added functionality to atm file as well as adding further tests to rspec. All tests pass.

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,9 @@ class Atm
     def initialize
         @funds = 1000
     end
+
+    def withdrawl(amount)
+        @funds -= amount
+    end
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -4,4 +4,18 @@ describe Atm do
     it 'has 1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
+
+    it 'funds are withdrew at withdrawl' do
+        subject.withdrawl 50
+        expect(subject.funds).to eq 950
+    end
+
+
+
+
+
+
+
+
 end
+
