3f0aa43d758e5a882ca131babd3f37f146e92e35
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-31 21:39:38 +0200

    added implementation of account owner all tests passed

--- lib/account.rb
+++ lib/account.rb
@@ -2,30 +2,44 @@ require 'date'
 
 class Account 
 
-    attr_accessor :exp_date, :account_status
-    attr_reader :pin_code
+    attr_accessor :exp_date, :account_status 
+    attr_reader :pin_code, :owner
 
     STANDARD_VALIDILITY_YRS = 5 
 
-    def initialize
+    def initialize(attrs = {})
         @exp_date = '03/26'
         @pin_code = rand(1000..9999)
         @account_status = :active
+        set_owner(attrs[:owner])
+        
     end
 
     def  deactivate
         @account_status = :deactivated
     end
 
+    def set_expire_date
+        
+        Date.today.next_year(STANDARD_VALIDILITY_YRS).strftime('%m/%y')
+        
+    end
 end
 
 
+private
 
-def set_expire_date
-    
-    Date.today.next_year(STANDARD_VALIDILITY_YRS).strftime('%m/%y')
-    
-end
+    def set_owner(obj)
+        obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner
+        raise "An account owner is required"
+    end
+
+
+
+
 
 
 
