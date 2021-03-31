e73f880917fd893d0a15e4389dc01ce38b3d3d2d
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-31 21:15:46 +0200

    added testing for account owner

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,17 @@ require './lib/account'
 
 
 describe Account do
+    let(:person) {instance_double('Person', name: 'Thomas')}
+    subject {described_class.new({owner: person}) }
     
+it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+end
+
+it 'is expected to raise an error if no owner is set' do
+    expect {described_class.new}.to raise_error 'An account owner is required'
+end
+
 it 'it is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
